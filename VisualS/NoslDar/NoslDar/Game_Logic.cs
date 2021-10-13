516a6970515d796d2e05c846b5a0dd963894c1b7
Edvarts Krakts <edvarts.krakts@gmail.com>
2021-10-13 21:48:22 +0300

    sākums

---
+++ VisualS/NoslDar/NoslDar/Game_Logic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoslDar
+{
+    public class Game_Logic
+    
+        
+        
+        Console.WriteLine();
+
+        1x
+        if One = 1x, tad 
+          {
+               if (1, 2, 3 visur ir vienādas vērtības (X vai 0), tad = uzvara)
+            if (4, 5,6 visur ir vienādas vērtības, tad = uzvara )
+            if (1, 5, 9  visur ir vienādas vērtības, tad = uzvara )
+            // ja One = 1, tad ir brīvs. 
+            //Bet, ja One ir aizstāts jau ar X vai 0, tad ir aizņemts.
+            //Ja visi laukumi ir pilni un nav 3 rindā, tad ir neizšķirts.
+
+
+    }
+}
+
--- VisualS/NoslDar/NoslDar/Program.cs
+++ VisualS/NoslDar/NoslDar/Program.cs
@@ -2,11 +2,24 @@
 
 namespace NoslDar
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
+
+        
+            //visas konsoles operācijas (X un 0 ievade) - lai paliek iekš Main. Un uz šo Main ir atsauces uz Loģikas klasi. 
+            // citā klasē - spēles loģika
         {
-            Console.WriteLine("Hello World!");
+            int One = 1;
+          
+
+
+
+            Console.WriteLine("    " + One + "  |    |  ");
+            Console.WriteLine("  --------------");
+            Console.WriteLine("      |    |  ");
+            Console.WriteLine("  --------------");
+            Console.WriteLine("      |    |  ");
 
             //Edvarts testē
         }
