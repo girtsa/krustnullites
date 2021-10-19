37d2628003b262b7107cf869dc5812a8d7fde335
Edvarts Krakts <edvarts.krakts@gmail.com>
2021-10-19 14:42:41 +0300

    edv v2

---
+++ Nosl_d_edv/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NoslDar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+
+
+        //visas konsoles operācijas (X un 0 ievade) - lai paliek iekš Main. Un uz šo Main ir atsauces uz Loģikas klasi. 
+        // citā klasē - spēles loģika
+
+
+
+        {
+            int one = 1;
+            int two = 2;
+            int three = 3;
+            int four = 4;
+            int five = 5;
+            int six = 6;
+            int seven = 7;
+            int eight = 8;
+            int nine = 9;
+
+
+
+
+
+            Console.WriteLine(one + "  | " + two + "   | " + three + ");
+            Console.WriteLine("  --------------");
+            Console.WriteLine(four + "  | " + five + "   | " + six + ");
+            Console.WriteLine("  --------------");
+            Console.WriteLine(seven + "  | " + eight + "   | " + nine + ");
+
+            //Edvarts testē
+        }
+    }
+}
