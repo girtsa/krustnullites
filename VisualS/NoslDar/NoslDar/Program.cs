29aa9d566582e2b4d5015da002d71ca00057f37e
Edvarts Krakts <edvarts.krakts@gmail.com>
2021-10-19 14:29:00 +0300

    edv 19 okt

--- VisualS/NoslDar/NoslDar/Program.cs
+++ VisualS/NoslDar/NoslDar/Program.cs
@@ -9,17 +9,29 @@ namespace NoslDar
         
             //visas konsoles operācijas (X un 0 ievade) - lai paliek iekš Main. Un uz šo Main ir atsauces uz Loģikas klasi. 
             // citā klasē - spēles loģika
+
+
+
         {
-            int One = 1;
+            int one = 1;
+            int two = 2;
+            int three = 3;
+            int four = 4;
+            int five = 5;
+            int six = 6;
+            int seven = 7;
+            int eight = 8;
+            int nine = 9;
+
           
 
 
 
-            Console.WriteLine("    " + One + "  |    |  ");
+            Console.WriteLine( one + "  | " + two + "   | " + three +  ");
             Console.WriteLine("  --------------");
-            Console.WriteLine("      |    |  ");
+            Console.WriteLine( four + "  | " + five + "   | " + six +  ");
             Console.WriteLine("  --------------");
-            Console.WriteLine("      |    |  ");
+            Console.WriteLine( seven + "  | " + eight + "   | " + nine +  ");
 
             //Edvarts testē
         }
